67f51da4719219c88e8baf5662135b7d567adb05
Pricilla Souza <pricilla_souzacs@hotmail.com>
2020-03-04 00:07:25 -0300

    subindo ex da calculadora

--- EX_GENERATION/calculatorBrain.js
+++ EX_GENERATION/calculatorBrain.js
@@ -17,31 +17,57 @@ function onNumberClicked(numberButton) {
 
 function onClearClicked() {
     result.innerHTML = "0";
-    n1=0
-    n2=0
-   
+    n1 = 0
+    n2 = 0
+
 }
 
 function onEqualsClicked() {
+   
     n2 = parseInt(result.innerHTML)
-    if (operacao == "+"){
-        result.innerHTML = n1+n2
+
+    if (operacao == "cos") { 
+        result.innerHTML = Math.cos(n1*(Math.PI/180))
     }
-    if (operacao == "-"){
-        result.innerHTML = n1-n2
+
+    if (operacao == "sen") {
+        result.innerHTML = Math.sin(n1*(Math.PI/180))
     }
-    if (operacao == "*"){
-        result.innerHTML = n1*n2
+
+    if (operacao == "raiz") {
+        result.innerHTML = Math.sqrt(n1)
     }
-    if (operacao == "/"){
-        if(n2>0){
-            result.innerHTML = n1/n2
+
+    if (operacao == "!*") {
+        var fatorial =1
+        result.innerHTML = 
+        for(var i=0; i<n1; i++){
+            fatorial=fatorial*[i]
         }
-        else{
-            result.innerHTML = "ZERO não é divisível"
-        }        
+
+        }
+      
+    if (operacao == "+") {
+        result.innerHTML = n1 + n2
     }
-    
+
+    if (operacao == "-") {
+        result.innerHTML = n1 - n2
+    }
+
+    if (operacao == "*") {
+        result.innerHTML = n1 * n2
+    }
+
+    if (operacao == "/") {
+        if (n2 > 0) {
+            result.innerHTML = n1 / n2
+        }
+        else {
+            result.innerHTML = "ZERO não é divisível"
+        }
+    }
+
 }
 
 function onDivideClicked() {
@@ -49,35 +75,54 @@ function onDivideClicked() {
     n1 = parseInt(n1)
     operacao = "/"
     result.innerHTML = 0
-    
 }
 
 function onAddClicked() {
     n1 = result.innerHTML
-    n1 = parseInt(n1) 
-    operacao = "+" 
+    n1 = parseInt(n1)
+    operacao = "+"
     result.innerHTML = 0
 }
 
 function onSubtractClicked() {
     n1 = result.innerHTML
-    n1 = parseInt(n1) 
-    operacao = "-" 
+    n1 = parseInt(n1)
+    operacao = "-"
     result.innerHTML = 0
-    
+
 }
 
 function onMultiplyCliked() {
     n1 = result.innerHTML
-    n1 = parseInt(n1) 
-    operacao = "*" 
-    result.innerHTML = 0    
+    n1 = parseInt(n1)
+    operacao = "*"
+    result.innerHTML = 0
 }
 
-function onRootCliked() {
+function onCosenoCliked() {
     n1 = result.innerHTML
-    n1 = parseInt(n1) 
-    operacao = "" 
-    result.innerHTML = 0    
+    n1 = parseInt(n1)
+    operacao = "cos"
+    result.innerHTML = 0
 }
 
+function onSenoCliked() {
+    n1 = result.innerHTML
+    n1 = parseInt(n1)
+    operacao = "sen"
+    result.innerHTML = 0
+}
+
+function onRaizQuadradaCliked() {
+    n1 = result.innerHTML
+    n1 = parseInt(n1)
+    operacao = "raiz"
+    result.innerHTML = 0
+}
+
+function onPotenciaCliked() {
+    n1 = result.innerHTML
+    n1 = parseInt(n1)
+    operacao = "!*"
+    result.innerHTML = 0
+}
